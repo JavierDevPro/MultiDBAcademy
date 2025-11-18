11e1b06b63dfcecd15e386a660db4c9c4692ebf6
mateocasta33 <162051608+mateocasta33@users.noreply.github.com>
2025-11-18 12:21:12 -0500

    The user entity was modified

--- MultiDBAcademy.Domain/Entities/User.cs
+++ MultiDBAcademy.Domain/Entities/User.cs
@@ -1,21 +1,36 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MultiDBAcademy.Domain.Entities;
 
 public class User
-{
-    public int Id { get; set; }
+{ 
+    [Key]
+    public Guid Id { get; set; }
+    
+    [Required]
     public string UserName { get; set; }
+    
+    [EmailAddress(ErrorMessage = "Email is not valid")]
+    [Required(ErrorMessage = "Email is required")]
     public string Email { get; set; }
     
+    [Required(ErrorMessage = "Role id is required")]
     public int RoleId { get; set; }
+    
     [ForeignKey("RoleId")]
+    [Required(ErrorMessage = "Role is required")]
     public Role Role { get; set; }
     
+    [Required(ErrorMessage = "Password is required")]
     public string PassHash { get; set; }
+    
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpire { get; set; }
+    
     public DateTime CreateAt {get; set;}
     public DateTime UpdateAt { get; set; }
-    public DateTime RefreshTokenExpire { get; set; }
+    
 
     public List<InstanceDB> InstancesDB = new List<InstanceDB>();
     public List<Email> Emails = new List<Email>();
